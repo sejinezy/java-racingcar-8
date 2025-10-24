e87dab3d9260a92102537c0a7dc39a10c99c6a62
sejin <psj8880@naver.com>
2025-10-24 17:28:00 +0900

    feat(Application): 프로그램 흐름 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,34 @@
 package racingcar;
 
+import java.util.ArrayList;
+import racingcar.domain.Number;
+import racingcar.domain.ParticipatingCars;
+import racingcar.service.RacingGame;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+
+        String carNamesInput = InputView.carNamesReadLine();
+        InputView.validateBlank(carNamesInput);
+
+        String numberInput = InputView.numberReadLine();
+        InputView.validateBlank(numberInput);
+        Number number = new Number(numberInput);
+
+        String[] parsed = InputView.parse(carNamesInput);
+
+        ParticipatingCars participatingCars = new ParticipatingCars(parsed);
+
+
+        RacingGame racingGame = new RacingGame(participatingCars);
+        GameEngine gameEngine = new GameEngine();
+        gameEngine.runAll(number, racingGame);
+        ArrayList<String> winner = gameEngine.getWinner(participatingCars);
+
+        OutputView.printWinner(winner);
+
     }
 }
