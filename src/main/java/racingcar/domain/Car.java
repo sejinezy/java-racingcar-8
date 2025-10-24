fea508f706cda2e2a48bac72a5264429f61542a1
sejin <psj8880@naver.com>
2025-10-24 17:15:06 +0900

    feat(Car): 자동차 도메인 구현
    - 이름과 위치 상태를 가진다
    - 이름에 대한 검증 (5자 이하)
    - 전진하는 기능

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,31 @@
+package racingcar.domain;
+
+public class Car {
+
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    private void validate(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+        }
+    }
+
+    public void moveForward() {
+        position++;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
