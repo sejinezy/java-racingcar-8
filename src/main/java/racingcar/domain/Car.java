41d81190b0831fa67fa45472a2c260f852ddc827
sejin <psj8880@naver.com>
2025-10-25 23:18:15 +0900

    refactor(Car): 상수명 수정

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,7 +3,7 @@ package racingcar.domain;
 public class Car {
 
     private static final String ERR_CAR_NAME_TOO_LONG = "이름은 5자 이하만 가능합니다.";
-    private static final int MIN_CAR_NAME_LENGTH = 5;
+    private static final int MAX_CAR_NAME_LENGTH = 5;
 
     private final String name;
     private int position;
@@ -14,7 +14,7 @@ public class Car {
     }
 
     private void validateLength(String name) {
-        if (name.length() > MIN_CAR_NAME_LENGTH) {
+        if (name.length() > MAX_CAR_NAME_LENGTH) {
             throw new IllegalArgumentException(ERR_CAR_NAME_TOO_LONG);
         }
     }
