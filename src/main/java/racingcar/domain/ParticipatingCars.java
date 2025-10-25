b1d67bd62fbbb6f47310b2d91bc39e00f1c26264
sejin <psj8880@naver.com>
2025-10-25 11:54:26 +0900

    refactor(ParticipatingCars): 배열 대신 컬렉션 사용 / 불변 컬렉션 반환
    - 메서드의 매개변수형을 배열에서 컬렉션으로 리팩터링
    - 안전성을 위해 필드를 불변으로 두고, 반환하는 get메소드에서도 불변으로
    반환하도록 리팩터링

--- src/main/java/racingcar/domain/ParticipatingCars.java
+++ src/main/java/racingcar/domain/ParticipatingCars.java
@@ -1,14 +1,16 @@
 package racingcar.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 
+
 public class ParticipatingCars {
 
-    private List<Car> cars = new ArrayList<>();
+    private final List<Car> cars = new ArrayList<>();
 
-    public ParticipatingCars(String[] cars) {
+    public ParticipatingCars(List<String> cars) {
         validateCars(cars);
         validateSize(cars);
         for (String car : cars) {
@@ -16,20 +18,20 @@ public class ParticipatingCars {
         }
     }
 
-    private void validateCars(String[] cars) {
-        List<String> list = List.of(cars);
-        if (list.size() != new HashSet<>(list).size()) {
+    private void validateCars(List<String> cars) {
+        if (cars.size() != new HashSet<>(cars).size()) {
             throw new IllegalArgumentException("한 게임의 자동차 이름은 모두 달라야 합니다.");
         }
     }
 
-    private void validateSize(String[] cars) {
-        if (cars.length < 2) {
+    private void validateSize(List<String> cars) {
+        if (cars.size() < 2) {
             throw new IllegalArgumentException("한 게임의 자동차는 2대 이상이어야 합니다.");
         }
     }
 
     public List<Car> getCars() {
-        return cars;
+        return Collections.unmodifiableList(cars);
     }
+
 }
