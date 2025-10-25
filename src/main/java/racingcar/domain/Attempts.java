5e608aa9f7144f23f67536cd19c4fc92c9cbd4f3
sejin <psj8880@naver.com>
2025-10-25 15:28:28 +0900

    refactor(Attempts): 의도를 드러내도록 메서드명 리팩터링 / 중복 검증 삭제

--- src/main/java/racingcar/domain/Attempts.java
+++ src/main/java/racingcar/domain/Attempts.java
@@ -4,15 +4,15 @@ public class Attempts {
 
     private final int number;
 
-    public Attempts(String rawInput) {
-        int parsedInt = parseInt(rawInput);
-        validate(parsedInt);
-        this.number = parsedInt;
+    public Attempts(String numberInput) {
+        int number = parseInt(numberInput);
+        validatePositive(number);
+        this.number = number;
     }
 
-    private static int parseInt(String number) {
+    private static int parseInt(String numberInput) {
         try {
-            return Integer.parseInt(number.trim());
+            return Integer.parseInt(numberInput);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("정수 숫자만 가능합니다.");
         }
@@ -22,7 +22,7 @@ public class Attempts {
         return number > 0;
     }
 
-    private void validate(int number) {
+    private void validatePositive(int number) {
         if (!isPositive(number)) {
             throw new IllegalArgumentException("1 이상이어야 합니다.");
         }
